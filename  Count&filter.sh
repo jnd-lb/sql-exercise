b07cb44f3a3ea0b682d8560c24dbc55ec7b176a7
Jihad Noureddine <jihad.nd@gmail.com>
2020-10-17 08:39:11 +0300

    Count and Filter

---  Count&filter.sh
+++  Count&filter.sh
@@ -3,3 +3,5 @@ SELECT AVG(Points) As 'average point' from students;
 SELECT COUNT(Points) As 'Number of student that has 500' from students where Points = 500;
 SELECT Name from students where Name Like "%s%";
 SELECT * from students ORDER by Points DESC;
+
+
